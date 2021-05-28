a5a04a617caf5a025e9def8a2d9cd1c0211e3291
neronix17 <39186138+neronix17@users.noreply.github.com>
2021-05-29 00:32:42 +0100

    DeathActionWorker_Spawner & FleshType: Husk
    - Added DeathActionWorker_Spawner & DefModExt_DeathSpawnerProps C#
    - Added FleshType: Husk (RE_Husk)
    - Added ThingCategory: Husk Corpses (RE_HuskCorpses)

---
+++ Source/RimEffectReapers/DeathActionWorker_Spawner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace RimEffectReapers
+{
+    public class DeathActionWorker_Spawner : DeathActionWorker
+    {
+        public override void PawnDied(Corpse corpse)
+        {
+            DefModExt_DeathSpawnerProps props = corpse.InnerPawn?.def?.GetModExtension<DefModExt_DeathSpawnerProps>() ?? null;
+
+            if(props == null)
+            {
+                Log.Warning("DeathActionWorker_Spawner used without required modExtension, meaning it will do nothing.");
+                return;
+            }
+
+            int spawnCount = props.quantityRange.RandomInRange;
+
+            PawnGenerationRequest request = new PawnGenerationRequest(
+                kind: props.kindDef,
+                faction: corpse.InnerPawn.Faction,
+                newborn: true,
+                forceGenerateNewPawn: true,
+                canGeneratePawnRelations: false
+            );
+
+            for (int i = 0; i < spawnCount; i++)
+            {
+                Pawn newThing = PawnGenerator.GeneratePawn(request);
+                GenSpawn.Spawn(newThing, corpse.Position, corpse.Map, WipeMode.Vanish);
+            }
+        }
+    }
+}
