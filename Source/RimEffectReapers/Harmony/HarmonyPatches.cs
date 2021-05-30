900b26981e7c0f9b68b7d2ed64bf01735bae1182
neronix17 <39186138+neronix17@users.noreply.github.com>
2021-05-30 12:05:52 +0100

    FleshType Code
    - Husks now affected by EMP
    - Husks no longer rot
    - Husks are no longer ingestible

---
+++ Source/RimEffectReapers/Harmony/HarmonyPatches.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+using HarmonyLib;
+
+namespace RimEffectReapers
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyInit
+    {
+        static HarmonyInit()
+        {
+            new Harmony("OskarPotocki.RimEffectReapers").PatchAll();
+        }
+    }
+}
