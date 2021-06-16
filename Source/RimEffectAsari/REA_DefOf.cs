211a408d94def7f576f89a9df22eba36a9fd7d72
Simon Latusek <simon.latusek@gmail.com>
2021-06-16 17:50:43 +0200

    Charmed ability

---
+++ Source/RimEffectAsari/REA_DefOf.cs
@@ -0,0 +1,12 @@
+﻿namespace RimEffectAsari
+{
+    using RimWorld;
+    using Verse;
+
+    [DefOf]
+    public static class REA_DefOf
+    {
+        public static HediffDef RE_Biotic_CharmedHediff;
+        public static HediffDef RE_Biotic_CharmHediff;
+    }
+}
