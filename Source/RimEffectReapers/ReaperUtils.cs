fb7f5725421193b83acaeef1eabc093a781a160f
Kylian <kylian.balde@gmail.com>
2021-08-17 10:37:38 +0200

    Fix reaper arrival and reaper base attack

--- Source/RimEffectReapers/ReaperUtils.cs
+++ Source/RimEffectReapers/ReaperUtils.cs
@@ -35,44 +35,6 @@ namespace RimEffectReapers
                 s.Faction.def == RER_DefOf.RE_Reapers && s.def == RER_DefOf.RE_Reaper) * 2000;
         }
 
-        public static void HeightWidthFromLayout(StructureLayoutDef structureLayoutDef, out int height, out int width)
-        {
-            if (structureLayoutDef == null || structureLayoutDef.layouts.Count == 0)
-            {
-                Log.Warning("StructureLayoutDef was null or empty. Throwing 10 10 size");
-                height = 10;
-                width = 10;
-                return;
-            }
-
-            height = structureLayoutDef.layouts[0].Count;
-            width = structureLayoutDef.layouts[0][0].Split(',').ToList().Count;
-        }
-
-        public static Dictionary<string, SymbolDef> FillpairsSymbolLabel()
-        {
-            var pairsSymbolLabel = new Dictionary<string, SymbolDef>();
-            var symbolDefs = DefDatabase<SymbolDef>.AllDefsListForReading;
-            foreach (var s in symbolDefs) pairsSymbolLabel.Add(s.symbol, s);
-
-            return pairsSymbolLabel;
-        }
-
-        public static IntVec3 FindRect(Map map, int maxTries)
-        {
-            var c = 0;
-            while (true)
-            {
-                var rect = CellRect.CenteredOn(CellFinder.RandomNotEdgeCell(33, map), 33, 33);
-                if (!rect.Cells.ToList().Any(i =>
-                    !i.Walkable(map) || !i.GetTerrain(map).affordances.Contains(TerrainAffordanceDefOf.Medium)))
-                    return rect.CenterCell;
-
-                if (c > maxTries) return IntVec3.Invalid;
-                c++;
-            }
-        }
-
         public static bool HasAnyOtherBase(Settlement defeatedFactionBase)
         {
             var settlements = Find.WorldObjects.Settlements;
--- Source/RimEffectReapers/Reaper_Skyfaller.cs
+++ Source/RimEffectReapers/Reaper_Skyfaller.cs
@@ -1,13 +1,11 @@
-﻿using System.Linq;
+﻿using KCSG;
 using RimWorld;
-using UnityEngine;
 using Verse;
 
 namespace RimEffectReapers
 {
-    public class Reaper_Skyfaller : Skyfaller
+    public class Reaper_Skyfaller : KCSG_Skyfaller
     {
-        private static readonly IntRange range = new IntRange(-3, 3);
         private GameCondition caused;
 
         public override void SpawnSetup(Map map, bool respawningAfterLoad)
@@ -38,40 +36,6 @@ namespace RimEffectReapers
         {
             base.ExposeData();
             Scribe_References.Look(ref caused, "caused");
-        }
-
-        public override void DrawAt(Vector3 drawLoc, bool flip = false)
-        {
-            innerContainer[0]?.DrawAt(drawLoc, flip);
-        }
-
-        protected override void SpawnThings()
-        {
-            foreach (var t in innerContainer)
-                GenPlace.TryPlaceThing(t, Position, Map, ThingPlaceMode.Direct,
-                    delegate(Thing thing, int count)
-                    {
-                        PawnUtility.RecoverFromUnwalkablePositionOrKill(thing.Position, thing.Map);
-                        if (thing.def.Fillage == FillCategory.Full && def.skyfaller.CausesExplosion &&
-                            def.skyfaller.explosionDamage.isExplosive &&
-                            thing.Position.InHorDistOf(Position, def.skyfaller.explosionRadius))
-                            Map.terrainGrid.Notify_TerrainDestroyed(thing.Position);
-                    }, null, Rotation);
-
-            foreach (var thing in innerContainer)
-            foreach (var t in GenAdj.OccupiedRect(Position, Rotation, thing.def.size).SelectMany(
-                cell =>
-                    cell.GetThingList(Map)).Except(innerContainer))
-                t.Destroy();
-        }
-
-        public override void Tick()
-        {
-            base.Tick();
-            if (Rand.Chance(0.001f) && this.IsHashIntervalTick(60))
-                Map.weatherManager.eventHandler.AddEvent(
-                    new WeatherEvent_ReaperLightningStrike(Find.CurrentMap,
-                        Position + new IntVec3(range.RandomInRange, 0, range.RandomInRange)));
         }
     }
 }
