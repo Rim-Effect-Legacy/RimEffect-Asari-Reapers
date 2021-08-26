c54ad19b1a6a258dc233ec442007f2d876e6ccf4
Simon Latusek <simon.latusek@gmail.com>
2021-08-26 17:02:43 +0200

    slight fix for ardat yakshi stat

--- Source/RimEffectAsari/StatWorker_ArdatYakshiPower.cs
+++ Source/RimEffectAsari/StatWorker_ArdatYakshiPower.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RimEffectAsari
+﻿namespace RimEffectAsari
 {
     using RimWorld;
     using Verse;
 
     public class StatWorker_ArdatYakshiPower : StatWorker
     {
+        public override bool ShouldShowFor(StatRequest req) => base.ShouldShowFor(req) && ((req.Thing as Pawn)?.story?.traits?.HasTrait(REA_DefOf.RE_ArdatYakshi) ?? false);
+
         public override float GetValueUnfinalized(StatRequest req, bool applyPostProcess = true)
         {
             float valueUnfinalized = base.GetValueUnfinalized(req, applyPostProcess);
 
-            valueUnfinalized *= (req.Thing as Pawn)?.health.hediffSet.GetFirstHediffOfDef(REA_DefOf.RE_ArdatYakshi_Power).Severity ?? 1;
+            if((req.Thing as Pawn)?.story?.traits?.HasTrait(REA_DefOf.RE_ArdatYakshi) ?? false)
+                valueUnfinalized *= (req.Thing as Pawn)?.health.hediffSet.GetFirstHediffOfDef(REA_DefOf.RE_ArdatYakshi_Power)?.Severity ?? 1;
 
             return valueUnfinalized;
         }
