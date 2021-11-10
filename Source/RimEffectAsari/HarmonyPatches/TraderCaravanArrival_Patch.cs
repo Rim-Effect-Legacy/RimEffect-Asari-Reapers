feba1e02ed8aba77ea96bef434306cde838feb5c
Joshua <legodude17@users.noreply.github.com>
2021-11-10 02:34:56 -0800

    Fix animals not going with caravan

--- Source/RimEffectAsari/HarmonyPatches/TraderCaravanArrival_Patch.cs
+++ Source/RimEffectAsari/HarmonyPatches/TraderCaravanArrival_Patch.cs
@@ -1,8 +1,8 @@
 ﻿namespace RimEffectAsari
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Reflection;
     using System.Reflection.Emit;
     using HarmonyLib;
     using RimWorld;
@@ -16,11 +16,11 @@
         [HarmonyTranspiler]
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            List<CodeInstruction> list   = instructions.ToList();
-            MethodInfo    info1  = AccessTools.Method(typeof(IncidentWorker_TraderCaravanArrival), "SendLetter");
-            int    idx1   = list.FindIndex(ins => ins.Calls(info1)) + 1;
-            Label    label1 = generator.DefineLabel();
-            Label    label2 = generator.DefineLabel();
+            var list   = instructions.ToList();
+            var info1  = AccessTools.Method(typeof(IncidentWorker_TraderCaravanArrival), "SendLetter");
+            var idx1   = list.FindIndex(ins => ins.Calls(info1)) + 1;
+            var label1 = generator.DefineLabel();
+            var label2 = generator.DefineLabel();
             list[idx1].labels.Add(label2);
             list.InsertRange(idx1, new[]
             {
@@ -31,7 +31,7 @@
                 new CodeInstruction(OpCodes.Bne_Un, label2),
                 new CodeInstruction(OpCodes.Ldarg_1),
                 new CodeInstruction(OpCodes.Ldloc_0),
-                new CodeInstruction(list[idx1-3]),
+                new CodeInstruction(list[idx1 - 3]),
                 new CodeInstruction(OpCodes.Ldloc_2),
                 new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(TraderCaravanArrival_Patch), nameof(TraderArrivalAsari))),
                 new CodeInstruction(OpCodes.Ldc_I4_1),
@@ -42,9 +42,9 @@
 
         public static void TraderArrivalAsari(IncidentParms parms, List<Pawn> pawns, TraderKindDef traderKind)
         {
-            AsariShuttleLanded shuttle = (AsariShuttleLanded) ThingMaker.MakeThing(REA_DefOf.RE_AsariShuttleLanded);
+            var shuttle = (AsariShuttleLanded) ThingMaker.MakeThing(REA_DefOf.RE_AsariShuttleLanded);
             shuttle.SetFactionDirect(parms.faction);
-            foreach (Pawn pawn in pawns) pawn.DeSpawn();
+            foreach (var pawn in pawns) pawn.DeSpawn();
             // pawns.RemoveAll(pawn =>
             // {
             //     if (!pawn.RaceProps.Animal) return false;
@@ -57,7 +57,7 @@
             shuttle.State    = AsariShuttleLanded.ShuttleState.Unloading;
             shuttle.Rotation = Rot4.East;
 
-            Map map = parms.target as Map;
+            var map = parms.target as Map;
             SkyfallerMaker.SpawnSkyfaller(REA_DefOf.RE_AsariShuttleIncoming, shuttle, DropCellFinder.GetBestShuttleLandingSpot(map, parms.faction), map).Rotation = Rot4.North;
         }
     }
@@ -95,7 +95,7 @@
                     if (lord == null)
                         lord = LordMaker.MakeNewLord(Faction,
                             new LordJob_TradeWithColonyFromShuttle(Faction, Position + new IntVec3(Rot4.Random.AsVector2) * Rand.Range(4, 7), this), Map);
-                    if (innerContainer.OfType<Pawn>().TryRandomElement(out Pawn pawn))
+                    if (innerContainer.OfType<Pawn>().TryRandomElement(out var pawn))
                     {
                         innerContainer.TryDrop(pawn, Position + IntVec3.South * 2, Map, ThingPlaceMode.Near, 1, out _);
                         if (pawn.GetLord() != null) pawn.GetLord().Notify_PawnLost(pawn, PawnLostCondition.ForcedToJoinOtherLord);
@@ -107,10 +107,18 @@
                     if (!innerContainer.Any()) State = ShuttleState.Waiting;
                     return;
                 case ShuttleState.Loading when ToLoad != null && !ToLoad.Any():
-                    Map map  = Map;
-                    IntVec3 cell = Position;
+                    var map  = Map;
+                    var cell = Position;
                     DeSpawn();
-                    SkyfallerMaker.SpawnSkyfaller(REA_DefOf.RE_AsariShuttleLeaving, this, cell, map);
+
+                    foreach (var thing in innerContainer)
+                        if (thing is Pawn p && p.GetLord() is Lord lord)
+                        {
+                            lord.RemovePawn(p);
+                            if (!lord.ownedPawns.Any()) lord.Cleanup();
+                        }
+
+                    SkyfallerMaker.SpawnSkyfaller(REA_DefOf.RE_AsariShuttleLeaving, this, cell, map).Rotation = Rot4.North;
                     return;
                 case ShuttleState.Waiting:
                     return;
@@ -128,11 +136,23 @@
         }
     }
 
+    public class AsariShuttleFlying : Skyfaller
+    {
+        protected override void LeaveMap()
+        {
+            foreach (var thing in innerContainer)
+                if (thing is Pawn p)
+                    p.DestroyOrPassToWorld();
+
+            base.LeaveMap();
+        }
+    }
+
     public class JobGiver_EnterAsariShuttle : ThinkNode_JobGiver
     {
         protected override Job TryGiveJob(Pawn pawn)
         {
-            AsariShuttleLanded shuttle = pawn.Map.listerThings.ThingsOfDef(REA_DefOf.RE_AsariShuttleLanded).OfType<AsariShuttleLanded>().FirstOrDefault(t =>
+            var shuttle = pawn.Map.listerThings.ThingsOfDef(REA_DefOf.RE_AsariShuttleLanded).OfType<AsariShuttleLanded>().FirstOrDefault(t =>
                 t.Faction == pawn.Faction && t.State == AsariShuttleLanded.ShuttleState.Loading && t.ToLoad != null && t.ToLoad.Contains(pawn));
             return shuttle != null ? JobMaker.MakeJob(REA_DefOf.RE_EnterShuttle, shuttle) : null;
         }
@@ -174,81 +194,83 @@
 
         public override StateGraph CreateGraph()
         {
-            StateGraph graph      = new StateGraph();
-            LordToil_Travel travelToil = new LordToil_Travel(chillSpot);
+            var graph      = new StateGraph();
+            var travelToil = new LordToil_Travel(chillSpot);
             graph.StartingToil = travelToil;
-            LordToil_DefendPoint chill = new LordToil_DefendPoint(chillSpot);
+            var chill = new LordToil_DefendTrader(chillSpot);
             graph.AddToil(chill);
-            LordToil_DefendPoint defendTrader = new LordToil_DefendPoint();
+            var defendTrader = new LordToil_DefendTrader();
             graph.AddToil(defendTrader);
-            LordToil_LeaveOnShuttle leaveNormal = new LordToil_LeaveOnShuttle();
+            var leaveNormal = new LordToil_LeaveOnShuttle();
             graph.AddToil(leaveNormal);
-            LordToil_LeaveOnShuttleAndDefendSelf leaveDefend = new LordToil_LeaveOnShuttleAndDefendSelf();
+            var leaveDefend = new LordToil_LeaveOnShuttleAndDefendSelf();
             graph.AddToil(leaveDefend);
-            LordToil_LeaveOnShuttle leaveMining = new LordToil_LeaveOnShuttle(LocomotionUrgency.Walk, true);
+            var leaveMining = new LordToil_LeaveOnShuttle(LocomotionUrgency.Walk, true);
             graph.AddToil(leaveMining);
-            Transition dangerousTemp = new Transition(travelToil, leaveNormal);
+            var dangerousTemp = new Transition(travelToil, leaveNormal);
             dangerousTemp.AddSource(chill);
             dangerousTemp.AddPreAction(new TransitionAction_Message("MessageVisitorsDangerousTemperature".Translate(faction.def.pawnsPlural.CapitalizeFirst(), faction.Name)));
             dangerousTemp.AddPostAction(new TransitionAction_EndAllJobs());
             dangerousTemp.AddTrigger(new Trigger_PawnExperiencingDangerousTemperatures());
             graph.AddTransition(dangerousTemp);
-            Transition trapped = new Transition(travelToil, leaveMining);
+            var trapped = new Transition(travelToil, leaveMining);
             trapped.AddSources(chill, leaveNormal, leaveDefend);
             trapped.AddPostAction(new TransitionAction_Message("MessageVisitorsTrappedLeaving".Translate(faction.def.pawnsPlural.CapitalizeFirst(), faction.Name)));
             trapped.AddPostAction(new TransitionAction_WakeAll());
             trapped.AddPostAction(new TransitionAction_EndAllJobs());
             trapped.AddTrigger(new Trigger_CannotReachShuttle());
             graph.AddTransition(trapped);
-            Transition notTrapped = new Transition(leaveMining, leaveDefend);
+            var notTrapped = new Transition(leaveMining, leaveDefend);
             notTrapped.AddTrigger(new Trigger_AllCanReachShuttle());
             notTrapped.AddPostAction(new TransitionAction_EndAllJobs());
             graph.AddTransition(notTrapped);
-            Transition defendOnHarmed = new Transition(travelToil, defendTrader);
+            var defendOnHarmed = new Transition(travelToil, defendTrader);
             defendOnHarmed.AddTrigger(new Trigger_PawnHarmed());
             defendOnHarmed.AddPreAction(new TransitionAction_SetDefendTrader());
             defendOnHarmed.AddPostAction(new TransitionAction_WakeAll());
             defendOnHarmed.AddPostAction(new TransitionAction_EndAllJobs());
             graph.AddTransition(defendOnHarmed);
-            Transition notHarmed = new Transition(defendTrader, leaveDefend);
+            var notHarmed = new Transition(defendTrader, leaveDefend);
             notHarmed.AddTrigger(new Trigger_TicksPassedWithoutHarm(1200));
             graph.AddTransition(notHarmed);
-            Transition arrive = new Transition(travelToil, chill);
+            var arrive = new Transition(travelToil, chill);
             arrive.AddTrigger(new Trigger_Memo("TravelArrived"));
             graph.AddTransition(arrive);
-            Transition leave = new Transition(chill, leaveNormal);
+            var leave = new Transition(chill, leaveNormal);
             leave.AddTrigger(new Trigger_TicksPassed(DebugSettings.instantVisitorsGift ? 2500 : Rand.Range(27000, 45000)));
             leave.AddPreAction(new TransitionAction_CheckGiveGift());
             leave.AddPreAction(new TransitionAction_Message("MessageTraderCaravanLeaving".Translate(faction.Name)));
             leave.AddPostAction(new TransitionAction_WakeAll());
             graph.AddTransition(leave);
-            Transition backup = new Transition(leaveDefend, leaveNormal);
+            var backup = new Transition(leaveDefend, leaveNormal);
             backup.AddSources(leaveMining, chill, defendTrader, travelToil);
             backup.AddTrigger(new Trigger_TicksPassed(60000));
             backup.AddPostAction(new TransitionAction_WakeAll());
             graph.AddTransition(backup);
-            Transition retreat = new Transition(defendTrader, leaveDefend);
+            var retreat = new Transition(defendTrader, leaveDefend);
             retreat.AddSources(chill, travelToil);
             retreat.AddTrigger(new Trigger_ImportantTraderCaravanPeopleLost());
             retreat.AddTrigger(new Trigger_BecamePlayerEnemy());
             retreat.AddPostAction(new TransitionAction_WakeAll());
             retreat.AddPostAction(new TransitionAction_EndAllJobs());
             graph.AddTransition(retreat);
-            LordToil_DefendPoint defendShuttle   = new LordToil_DefendPoint(Shuttle.Position, 7f);
-            Transition onShuttleDamage = new Transition(travelToil, defendShuttle);
+            var defendShuttle   = new LordToil_DefendPoint(Shuttle.Position, 7f);
+            var onShuttleDamage = new Transition(travelToil, defendShuttle);
             onShuttleDamage.AddSources(chill, defendTrader, leaveNormal, leaveMining, leaveDefend);
             onShuttleDamage.AddPostAction(new TransitionAction_WakeAll());
             onShuttleDamage.AddPostAction(new TransitionAction_EndAllJobs());
             onShuttleDamage.AddTrigger(new Trigger_ThingDamageTaken(Shuttle, 0.8f));
             graph.AddToil(defendShuttle);
             graph.AddTransition(onShuttleDamage);
-            Transition shuttleSafe = new Transition(defendShuttle, leaveDefend);
+            var shuttleSafe = new Transition(defendShuttle, leaveDefend);
             shuttleSafe.AddTrigger(new Trigger_TicksPassedWithoutHarm(1200));
             shuttleSafe.AddPostAction(new TransitionAction_WakeAll());
             shuttleSafe.AddPostAction(new TransitionAction_EndAllJobs());
             graph.AddTransition(shuttleSafe);
             return graph;
         }
+
+        public override bool CanOpenAnyDoor(Pawn p) => p.RaceProps.FenceBlocked;
 
         public override void ExposeData()
         {
@@ -258,13 +280,55 @@
         }
     }
 
+    public class LordToil_DefendTrader : LordToil_DefendPoint
+    {
+        public LordToil_DefendTrader()
+        {
+        }
+
+        public LordToil_DefendTrader(IntVec3 defendPoint) : base(defendPoint)
+        {
+        }
+
+        public override bool AllowSatisfyLongNeeds => false;
+
+        public override float? CustomWakeThreshold => 0.5f;
+
+        public override void UpdateAllDuties()
+        {
+            var pawn2 = lord.ownedPawns.Where(p => p.RaceProps.Humanlike).RandomElement();
+            foreach (var pawn in lord.ownedPawns)
+            {
+                Log.Message($"TraderCaravanRole of {pawn} is {pawn.GetTraderCaravanRole()}");
+                switch (pawn.GetTraderCaravanRole())
+                {
+                    case TraderCaravanRole.Carrier:
+                        pawn.mindState.duty            = new PawnDuty(DutyDefOf.Follow, pawn2, 5f);
+                        pawn.mindState.duty.locomotion = LocomotionUrgency.Walk;
+                        break;
+                    case TraderCaravanRole.Chattel:
+                        pawn.mindState.duty            = new PawnDuty(DutyDefOf.Escort, pawn2, 5f);
+                        pawn.mindState.duty.locomotion = LocomotionUrgency.Walk;
+                        break;
+                    case TraderCaravanRole.Trader:
+                    case TraderCaravanRole.Guard:
+                    case TraderCaravanRole.None:
+                        pawn.mindState.duty = new PawnDuty(DutyDefOf.Defend, Data.defendPoint, Data.defendRadius);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+
     public class Trigger_CannotReachShuttle : Trigger
     {
         public override bool ActivateOn(Lord lord, TriggerSignal signal)
         {
             if (signal.type == TriggerSignalType.Tick && Find.TickManager.TicksGame % 127 == 1)
             {
-                AsariShuttleLanded shuttle = ((LordJob_TradeWithColonyFromShuttle) lord.LordJob).Shuttle;
+                var shuttle = ((LordJob_TradeWithColonyFromShuttle) lord.LordJob).Shuttle;
                 if (lord.ownedPawns.Any(pawn => pawn.Spawned && !pawn.Downed && !pawn.Dead && !pawn.CanReach(shuttle, PathEndMode.Touch, Danger.Deadly))) return true;
             }
 
@@ -278,7 +342,7 @@
         {
             if (signal.type == TriggerSignalType.Tick && Find.TickManager.TicksGame % 127 == 1)
             {
-                AsariShuttleLanded shuttle = ((LordJob_TradeWithColonyFromShuttle) lord.LordJob).Shuttle;
+                var shuttle = ((LordJob_TradeWithColonyFromShuttle) lord.LordJob).Shuttle;
                 if (lord.ownedPawns.All(pawn => !pawn.Spawned || pawn.Downed || pawn.Dead && pawn.CanReach(shuttle, PathEndMode.Touch, Danger.Deadly))) return true;
             }
 
@@ -298,7 +362,7 @@
         public override void Init()
         {
             base.Init();
-            AsariShuttleLanded shuttle = ((LordJob_TradeWithColonyFromShuttle) lord.LordJob).Shuttle;
+            var shuttle = ((LordJob_TradeWithColonyFromShuttle) lord.LordJob).Shuttle;
             shuttle.ToLoad = lord.ownedPawns.ListFullCopy().Cast<Thing>().ToList();
             shuttle.State  = AsariShuttleLanded.ShuttleState.Loading;
         }
