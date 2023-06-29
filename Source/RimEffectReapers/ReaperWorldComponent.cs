b8f5b29d3e5ef6b4255e39625ff5e79f3d227ec9
Simon Latusek <simon.latusek@gmail.com>
2023-06-29 13:59:05 +0200

    once per day reaper invasion reeval

--- Source/RimEffectReapers/ReaperWorldComponent.cs
+++ Source/RimEffectReapers/ReaperWorldComponent.cs
@@ -20,27 +20,35 @@ namespace RimEffectReapers
         {
             base.WorldComponentTick();
 
-            if (!ReaperMod.settings.reaperInvasionIsDisabled && Find.TickManager.TicksGame >= this.nextReaperSpawn)
+            if (!ReaperMod.settings.reaperInvasionIsDisabled)
             {
-                this.lastReaperSpawn = Find.TickManager.TicksGame;
-
-                IncidentParms parms = StorytellerUtility.DefaultParmsNow(IncidentCategoryDefOf.ThreatBig, this.world);
-
-                if (ReaperMod.settings.reaperShipIncidentChances.TryRandomElementByWeight(kvp => kvp.Value, out KeyValuePair<string, float> incident))
+                if (Find.TickManager.TicksGame >= this.nextReaperSpawn && Find.TickManager.TicksGame % 60 == 0)
                 {
-                    IncidentDef def = DefDatabase<IncidentDef>.GetNamed(incident.Key, false);
-                    if (def is null)
-                    {
-                        ReaperMod.settings.reaperShipIncidentChances.Remove(incident.Key);
-                    }
-                    else if (def.Worker.CanFireNow(parms))
-                    {
-                        IncidentDef.Named(incident.Key).Worker.TryExecute(parms);
-                    }
-                }
+                    this.lastReaperSpawn = Find.TickManager.TicksGame;
 
-                int ticksTillNextInvasion = Mathf.Max(Mathf.RoundToInt(ReaperMod.settings.reaperTimeInterval.RandomInRange * (1000f * 1f / ReaperUtils.ReaperPresence())), GenDate.TicksPerHour / 4);
-                this.nextReaperSpawn = Find.TickManager.TicksGame + ticksTillNextInvasion;
+                    IncidentParms parms = StorytellerUtility.DefaultParmsNow(IncidentCategoryDefOf.ThreatBig, this.world);
+
+                    if (ReaperMod.settings.reaperShipIncidentChances.TryRandomElementByWeight(kvp => kvp.Value, out KeyValuePair<string, float> incident))
+                    {
+                        IncidentDef def = DefDatabase<IncidentDef>.GetNamed(incident.Key, false);
+                        if (def is null)
+                        {
+                            ReaperMod.settings.reaperShipIncidentChances.Remove(incident.Key);
+                        }
+                        else if (def.Worker.CanFireNow(parms))
+                        {
+                            IncidentDef.Named(incident.Key).Worker.TryExecute(parms);
+                        }
+                    }
+
+                    int ticksTillNextInvasion = Mathf.Max(Mathf.RoundToInt(ReaperMod.settings.reaperTimeInterval.RandomInRange * (1000f * 1f / ReaperUtils.ReaperPresence())), GenDate.TicksPerHour / 4);
+                    this.nextReaperSpawn = Find.TickManager.TicksGame + ticksTillNextInvasion;
+
+                } else if (Find.TickManager.TicksGame % GenDate.TicksPerDay == 0 && (this.nextReaperSpawn - this.lastReaperSpawn) > (ReaperMod.settings.reaperTimeInterval.max * 10f /* (1000 * 1 / 100)*/))
+                {
+                    int ticksTillNextInvasion = Mathf.Max(Mathf.RoundToInt(ReaperMod.settings.reaperTimeInterval.RandomInRange * (1000f * 1f / ReaperUtils.ReaperPresence())), GenDate.TicksPerHour / 4);
+                    this.nextReaperSpawn = this.lastReaperSpawn + ticksTillNextInvasion;
+                }
             }
         }
 
